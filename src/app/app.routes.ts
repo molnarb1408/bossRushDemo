a9905be11599f0b248346741794dbfa619d125bb
M.Bálint <157951130+molnarb1408@users.noreply.github.com>
2024-11-07 19:47:50 +0100

    copyright

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path: 'game', component: GameComponent},
+    {path: 'about', component: AboutComponent},
 ];
